d51287369c8551ffe87b597dee90d73fd0117226
Christopher Field <cfield@configit.com>
2016-07-02 16:33:41 +0100

    Implemented bootstrap tabs (#3)

--- app/app.component.js
+++ app/app.component.js
@@ -11,35 +11,39 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        this.CreateTeam = function (group, name, imageUrl, points) {
-            return { group: group, name: name, imageUrl: imageUrl, points: points };
+        this.selectedGroup = 'A';
+        this.selectGroup = function (group) {
+            this.selectedGroup = group;
+        };
+        this.CreateTeam = function (group, name, imageUrl, points, qualified) {
+            return { group: group, name: name, imageUrl: imageUrl, points: points, qualified: qualified };
         };
         this.groups = ['A', 'B', 'C', 'D', 'E', 'F'];
         this.teams = [
-            this.CreateTeam('A', 'France', '', 7),
-            this.CreateTeam('A', 'Switzerland', '', 5),
-            this.CreateTeam('A', 'Albania', '', 3),
-            this.CreateTeam('A', 'Romania', '', 1),
-            this.CreateTeam('B', 'Wales', '', 6),
-            this.CreateTeam('B', 'England', '', 5),
-            this.CreateTeam('B', 'Slovakia', '', 4),
-            this.CreateTeam('B', 'Russia', '', 1),
-            this.CreateTeam('C', 'Germany', '', 7),
-            this.CreateTeam('C', 'Poland', '', 7),
-            this.CreateTeam('C', 'Northern Ireland', '', 3),
-            this.CreateTeam('C', 'Ukraine', '', 0),
-            this.CreateTeam('D', 'Croatia', '', 7),
-            this.CreateTeam('D', 'Spain', '', 6),
-            this.CreateTeam('D', 'Turkey', '', 3),
-            this.CreateTeam('D', 'Czech Republic', '', 1),
-            this.CreateTeam('E', 'Italy', '', 6),
-            this.CreateTeam('E', 'Belgium', '', 6),
-            this.CreateTeam('E', 'Republic of Ireland', '', 4),
-            this.CreateTeam('E', 'Sweden', '', 1),
-            this.CreateTeam('F', 'Hungary', '', 5),
-            this.CreateTeam('F', 'Iceland', '', 5),
-            this.CreateTeam('F', 'Portugal', '', 3),
-            this.CreateTeam('F', 'Austria', '', 1)
+            this.CreateTeam('A', 'France', '', 7, 1),
+            this.CreateTeam('A', 'Switzerland', '', 5, 1),
+            this.CreateTeam('A', 'Albania', '', 3, 0),
+            this.CreateTeam('A', 'Romania', '', 1, 0),
+            this.CreateTeam('B', 'Wales', '', 6, 1),
+            this.CreateTeam('B', 'England', '', 5, 1),
+            this.CreateTeam('B', 'Slovakia', '', 4, 1),
+            this.CreateTeam('B', 'Russia', '', 1, 0),
+            this.CreateTeam('C', 'Germany', '', 7, 1),
+            this.CreateTeam('C', 'Poland', '', 7, 1),
+            this.CreateTeam('C', 'Northern Ireland', '', 3, 0),
+            this.CreateTeam('C', 'Ukraine', '', 0, 0),
+            this.CreateTeam('D', 'Croatia', '', 7, 1),
+            this.CreateTeam('D', 'Spain', '', 6, 1),
+            this.CreateTeam('D', 'Turkey', '', 3, 0),
+            this.CreateTeam('D', 'Czech Republic', '', 1, 0),
+            this.CreateTeam('E', 'Italy', '', 6, 1),
+            this.CreateTeam('E', 'Belgium', '', 6, 1),
+            this.CreateTeam('E', 'Republic of Ireland', '', 4, 1),
+            this.CreateTeam('E', 'Sweden', '', 1, 0),
+            this.CreateTeam('F', 'Hungary', '', 5, 1),
+            this.CreateTeam('F', 'Iceland', '', 5, 1),
+            this.CreateTeam('F', 'Portugal', '', 3, 1),
+            this.CreateTeam('F', 'Austria', '', 1, 0)
         ];
         this.teamsForGroup = function (group) {
             return this.teams.filter(function (team) {
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,42 +5,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  CreateTeam = function( group, name, imageUrl, points ) {
-    return { group: group, name: name, imageUrl: imageUrl, points: points }
+  selectedGroup = 'A';
+
+  selectGroup = function( group ) {
+    this.selectedGroup = group;
+  }
+
+  CreateTeam = function( group, name, imageUrl, points, qualified ) {
+    return { group: group, name: name, imageUrl: imageUrl, points: points, qualified: qualified }
   }
 
   groups = ['A', 'B', 'C', 'D', 'E', 'F' ];
 
   teams = [
-    this.CreateTeam( 'A', 'France', '', 7 ),
-    this.CreateTeam( 'A', 'Switzerland', '', 5 ),
-    this.CreateTeam( 'A', 'Albania', '', 3 ),
-    this.CreateTeam( 'A', 'Romania', '', 1 ),
+    this.CreateTeam( 'A', 'France', '', 7, 1 ),
+    this.CreateTeam( 'A', 'Switzerland', '', 5, 1 ),
+    this.CreateTeam( 'A', 'Albania', '', 3, 0 ),
+    this.CreateTeam( 'A', 'Romania', '', 1, 0 ),
     
-    this.CreateTeam( 'B', 'Wales', '', 6),
-    this.CreateTeam( 'B', 'England', '', 5 ),
-    this.CreateTeam( 'B', 'Slovakia', '', 4 ),
-    this.CreateTeam( 'B', 'Russia', '', 1 ),
+    this.CreateTeam( 'B', 'Wales', '', 6, 1 ),
+    this.CreateTeam( 'B', 'England', '', 5, 1 ),
+    this.CreateTeam( 'B', 'Slovakia', '', 4, 1 ),
+    this.CreateTeam( 'B', 'Russia', '', 1, 0 ),
  
-    this.CreateTeam( 'C', 'Germany', '', 7 ),
-    this.CreateTeam( 'C', 'Poland', '', 7 ),
-    this.CreateTeam( 'C', 'Northern Ireland', '', 3 ),
-    this.CreateTeam( 'C', 'Ukraine', '', 0 ),
+    this.CreateTeam( 'C', 'Germany', '', 7, 1 ),
+    this.CreateTeam( 'C', 'Poland', '', 7, 1 ),
+    this.CreateTeam( 'C', 'Northern Ireland', '', 3, 0 ),
+    this.CreateTeam( 'C', 'Ukraine', '', 0, 0 ),
  
-    this.CreateTeam( 'D', 'Croatia', '', 7 ),
-    this.CreateTeam( 'D', 'Spain', '', 6 ),
-    this.CreateTeam( 'D', 'Turkey', '', 3 ),
-    this.CreateTeam( 'D', 'Czech Republic', '', 1 ),
+    this.CreateTeam( 'D', 'Croatia', '', 7, 1 ),
+    this.CreateTeam( 'D', 'Spain', '', 6, 1 ),
+    this.CreateTeam( 'D', 'Turkey', '', 3, 0 ),
+    this.CreateTeam( 'D', 'Czech Republic', '', 1, 0 ),
  
-    this.CreateTeam( 'E', 'Italy', '', 6 ),
-    this.CreateTeam( 'E', 'Belgium', '', 6 ),
-    this.CreateTeam( 'E', 'Republic of Ireland', '', 4 ),
-    this.CreateTeam( 'E', 'Sweden', '', 1 ),
+    this.CreateTeam( 'E', 'Italy', '', 6, 1 ),
+    this.CreateTeam( 'E', 'Belgium', '', 6, 1 ),
+    this.CreateTeam( 'E', 'Republic of Ireland', '', 4, 1 ),
+    this.CreateTeam( 'E', 'Sweden', '', 1, 0 ),
  
-    this.CreateTeam( 'F', 'Hungary', '', 5 ),
-    this.CreateTeam( 'F', 'Iceland', '', 5 ),
-    this.CreateTeam( 'F', 'Portugal', '', 3 ),
-    this.CreateTeam( 'F', 'Austria', '', 1 )
+    this.CreateTeam( 'F', 'Hungary', '', 5, 1 ),
+    this.CreateTeam( 'F', 'Iceland', '', 5, 1 ),
+    this.CreateTeam( 'F', 'Portugal', '', 3, 1 ),
+    this.CreateTeam( 'F', 'Austria', '', 1, 0 )
   ];
 
   teamsForGroup = function( group) {
